cb3aa6abc7219c2348ba4e23f2e6bb0bdfe6ac1d
Luke Henshaw <lukehenshaw10@gmail.com>
2022-07-23 18:31:21 +0100

    Added NavBar and View Properties, Add Properties pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import '../styles/app.css';
+import "../styles/app.css";
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Properties} />
+          <Route exact path="/add-property" component={AddProperty} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
